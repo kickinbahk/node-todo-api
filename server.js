e206356e1383d796dfd4501b56cf1e7b94683122
kickinbahk <josiahmory@gmail.com>
2016-02-22 18:15:07 -0800

    add get req for todos array and single todo

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+    id: 1,
+    description: "Program Node app", 
+    completed: false    
+}, {
+    id: 2,
+    description: "Wash dishes",
+    completed: false
+}, {
+    id: 3,
+    description: "Do Taxes",
+    completed: true
+}];
 
 app.get('/', function(req,res) {
     res.send("Todo API Root");
+});
+
+app.get('/todos', function(req, res) {
+    res.json(todos);
+});
+
+app.get('/todos/:id', function(req, res) {
+    res.send(`Asking for todo with id of ${req.params.id}`);
 });
 
 app.listen(PORT, function() {
