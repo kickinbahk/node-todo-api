15b279b40ddda9917e987fa5ae0ccf338071874f
kickinbahk <kickinbahk@gmail.com>
2016-03-03 19:29:42 -0800

    Add post to var and increment counter

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
 var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.send('Todo API Root');
@@ -28,7 +32,12 @@ app.get('/todos/:id', function (req, res) {
 });
 
 app.post('/todos/', function (req, res) {
-
+  var body = req.body;
+  
+  body.id = todoNextId;
+  todos.push(body);
+  todoNextId += 1;
+  res.json(body);
 });
 
 app.listen(PORT, function () {
