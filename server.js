e1c499462acdf5ea781257de405a2b0270f72869
kickinbahk <kickinbahk@gmail.com>
2016-03-03 18:33:34 -0800

    adjust whitespace, add post method

--- server.js
+++ server.js
@@ -1,44 +1,37 @@
 var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-    id: 1,
-    description: "Program Node app", 
-    completed: false    
-}, {
-    id: 2,
-    description: "Wash dishes",
-    completed: false
-}, {
-    id: 3,
-    description: "Do Taxes",
-    completed: true
-}];
+var todos = [];
+var todoNextId = 1;
 
 app.get('/', function(req,res) {
-    res.send("Todo API Root");
+  res.send("Todo API Root");
 });
 
 app.get('/todos', function(req, res) {
-    res.json(todos);
+  res.json(todos);
 });
 
 app.get('/todos/:id', function(req, res) {
-    var todoId = Number(req.params.id);
-    var foundId;
-    for(var todo of todos) {
-        if (todo.id === todoId) {
-            foundId = todo;
-        }
+  var todoId = Number (req.params.id);
+  var foundId;
+  for (var todo of todos) {
+    if (todo.id === todoId) {
+      foundId = todo;
     }
-    if (foundId) {
-        res.json(foundId);
-    } else {
-        res.status(404).send();
-    }
+  }
+  if (foundId) {
+    res.json(foundId);
+  } else {
+    res.status(404).send();
+  }
+});
+
+app.post ('/todos/', function (req, res) {
+
 });
 
 app.listen(PORT, function() {
-    console.log(`Express listening on port ${PORT}...`);
+  console.log(`Express listening on port ${PORT}...`);
 });
    
