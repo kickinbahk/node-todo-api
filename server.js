236e26c6c60b43527e8b76179d32c6275b1d7e54
kickinbahk <kickinbahk@gmail.com>
2016-03-04 08:54:08 -0800

    remove semi colons to adhere to standard

--- server.js
+++ server.js
@@ -1,46 +1,45 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var _ = require('underscore');
+var express = require('express')
+var bodyParser = require('body-parser')
+var _ = require('underscore')
 
-var app = express();
-var PORT = process.env.PORT || 3000;
-var todos = [];
-var todoNextId = 1;
+var app = express()
+var PORT = process.env.PORT || 3000
+var todos = []
+var todoNextId = 1
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 app.get('/', function (req, res) {
-  res.send('Todo API Root');
-});
+  res.send('Todo API Root')
+})
 
 app.get('/todos', function (req, res) {
-  res.json(todos);
-});
+  res.json(todos)
+})
 
 app.get('/todos/:id', function (req, res) {
-  var todoId = Number(req.params.id);
-  var foundId = _.findWhere(todos, {id: todoId});
+  var todoId = Number(req.params.id)
+  var foundId = _.findWhere(todos, {id: todoId})
 
   if (foundId) {
-    res.json(foundId);
+    res.json(foundId)
   } else {
-    res.status(404).send();
+    res.status(404).send()
   }
-});
+})
 
 app.post('/todos/', function (req, res) {
-  var body = _.pick(req.body, 'description', 'completed');
+  var body = _.pick(req.body, 'description', 'completed')
   if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-    return res.status(400).send();
+    return res.status(400).send()
   }
-  body.description = body.description.trim();
-  body.id = todoNextId;
-  todos.push(body);
-  todoNextId += 1;
-  res.json(body);
-});
+  body.description = body.description.trim()
+  body.id = todoNextId
+  todos.push(body)
+  todoNextId += 1
+  res.json(body)
+})
 
 app.listen(PORT, function () {
-  console.log(`Express listening on port ${PORT}...`);
-});
-
+  console.log(`Express listening on port ${PORT}...`)
+})
