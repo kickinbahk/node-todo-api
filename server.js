48f9c297ad3db3ce5a2f3caa97eb655f2b23829a
kickinbahk <kickinbahk@gmail.com>
2016-03-05 12:52:26 -0800

    fix formatting

--- server.js
+++ server.js
@@ -16,11 +16,13 @@ app.get('/', function (req, res) {
 app.get('/todos', function (req, res) {
   var queryParams = req.query
   var filteredTodos = todos
+
   if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
     filteredTodos = _.where(filteredTodos, {completed: true})
   } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
     filteredTodos = _.where(filteredTodos, {completed: false})
   }
+
   res.json(filteredTodos)
 })
 
@@ -37,9 +39,11 @@ app.get('/todos/:id', function (req, res) {
 
 app.post('/todos/', function (req, res) {
   var body = _.pick(req.body, 'description', 'completed')
+
   if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
     return res.status(400).send()
   }
+
   body.description = body.description.trim()
   body.id = todoNextId
   todos.push(body)
@@ -50,6 +54,7 @@ app.post('/todos/', function (req, res) {
 app.delete('/todos/:id', function (req, res) {
   var todoId = Number(req.params.id)
   var foundTodoById = _.findWhere(todos, {id: todoId})
+
   if (!foundTodoById) {
     res.status(404).json({'error': 'No todo found with that id'})
   } else {
@@ -63,19 +68,23 @@ app.put('/todos/:id', function (req, res) {
   var validAttributes = {}
   var todoId = Number(req.params.id)
   var foundTodoById = _.findWhere(todos, {id: todoId})
+
   if (!foundTodoById) {
     return res.status(404).send()
   }
+
   if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
     validAttributes.completed = body.completed
   } else if (body.hasOwnProperty('completed')) {
     return res.status(400).send()
   }
+
   if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
     validAttributes.description = body.description
   } else if (body.hasOwnProperty('description')) {
     return res.status(400).send()
   }
+
   _.extend(foundTodoById, validAttributes)
   return res.json(foundTodoById)
 })
