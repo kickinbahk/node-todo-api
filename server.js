3e33910cc10400bf0f87dc4e54490cce6bd929b1
kickinbahk <kickinbahk@gmail.com>
2016-03-19 14:51:23 -0700

    add DELETE route to login

--- server.js
+++ server.js
@@ -152,6 +152,14 @@ app.post('/users/login', function (req, res) {
   })
 })
 
+app.delete('/users/login', middleware.requireAuthentication, function (req, res) {
+  req.token.destroy().then(function () {
+    res.status(204).send()
+  }).catch(function () {
+    res.status(500).send()
+  })
+})
+
 db.sequelize.sync({force: true}).then(function () {
   app.listen(PORT, function () {
     console.log(`Express listening on port ${PORT}...`)
